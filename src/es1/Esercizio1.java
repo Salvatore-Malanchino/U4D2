084acfa224e9786555d0501fcb3d213f7e5ebc41
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-03-05 16:37:12 +0100

    Esercizio1 completato

--- src/es1/Esercizio1.java
+++ src/es1/Esercizio1.java
@@ -7,15 +7,22 @@ public static boolean stringaPariDispari(String myString){
 return  myString.length() %2==0;
 
     }
-
+    public static boolean annoBisestile  (int myNumber){
+return myNumber %4==0 ||(myNumber %100==0 && myNumber %400==0);
+    }
     public static void main(String[] args) {
         Scanner myScanner=new Scanner(System.in);
-        System.out.print("inserisci la prima stringa");
-       String myFirstString= myScanner.nextLine();
-        if (stringaPariDispari(myFirstString)){
-            System.out.print("la stringa è pari");
-        }else { System.out.print("la stringa è dispari");}
+//        System.out.print("inserisci la prima stringa");
+//       String myFirstString= myScanner.nextLine();
+//        if (stringaPariDispari(myFirstString)){
+//            System.out.print("la stringa è pari");
+//        }else { System.out.print("la stringa è dispari");}
 
+        System.out.print("inserisci l'anno");
+int myNumber= myScanner.nextInt();
+if (annoBisestile(myNumber)){
+    System.out.print("l'anno è bisestile");
+}else{System.out.print("l'anno non è bisestile");}
 
 
 
