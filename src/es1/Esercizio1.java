33b3d372cfc4887b4acd2d108c595971e57f6aa5
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-03-05 18:20:05 +0100

    Esercizio 2 completato

--- src/es1/Esercizio1.java
+++ src/es1/Esercizio1.java
@@ -8,7 +8,7 @@ return  myString.length() %2==0;
 
     }
     public static boolean annoBisestile  (int myNumber){
-return myNumber %4==0 ||(myNumber %100==0 && myNumber %400==0);
+return (myNumber %4==0 )||(myNumber %100==0 && myNumber %400==0);
     }
     public static void main(String[] args) {
         Scanner myScanner=new Scanner(System.in);
@@ -19,7 +19,7 @@ return myNumber %4==0 ||(myNumber %100==0 && myNumber %400==0);
        }else { System.out.print("la stringa è dispari");}
 
         System.out.print("inserisci l'anno");
-int myNumber= myScanner.nextInt();
+int myNumber = myScanner.nextInt();
 if (annoBisestile(myNumber)){
     System.out.print("l'anno è bisestile");
 }else{System.out.print("l'anno non è bisestile");}
--- src/es2/Esercizio2.java
+++ src/es2/Esercizio2.java
@@ -1,7 +1,56 @@
 package es2;
 
+import java.util.Scanner;
+
 public class Esercizio2 {
+
+
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("inserisci il tuo numero");
+        int y = myScanner.nextInt();
+        switch (y) {
+            case 0: {
+                System.out.print("Questo vale 0");
+                break;
+            }
+            case 1: {
+                System.out.print("Questo vale 1");
+                break;
+            }
+            case 2: {
+                System.out.print("Questo vale 2");
+                break;
+            }
+            case 3: {
+                System.out.print("Questo vale 3");
+                break;
+            }
+            default: {
+                System.out.print("nessuna delle precedenti");
+            }
+        }
 
     }
 }
+
+// SWTICH CASE + SCANNNER
+//        Scanner myScanner = new Scanner((System.in));
+//        System.out.print("Dammi X");
+//        int x= myScanner.nextInt();
+//
+//        System.out.print("Dammi Y");
+//        int y= myScanner.nextInt();
+//        int z = x>y ? x: y;
+//        switch (z) {
+//            case 10:{ System.out.print("z è 10");
+//            break;}
+//            case 100:{ System.out.print("z è 100");
+//            break;}
+//            case 1000:{ System.out.print("z è 10000");
+//            break;}
+//            default:{System.out.print("z non è nessuno di questi");
+//            break;}
+//
+//            }
+//            myScanner.close();
